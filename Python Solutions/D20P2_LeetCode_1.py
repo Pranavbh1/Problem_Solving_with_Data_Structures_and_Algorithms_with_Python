4e21a6e972a2c9067e6ee0c9d4af667bfd9c80e4
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-19 10:04:41 +0530

    Update D20P2_LeetCode_1.py
    In this code we are initialising a empty dict then we are enumerating on nums to get index and values , and checking that if the condition i.e. target - number is in empty dict , if not then updating the dict .
    At last we are print the value for our desired condition with its index .

--- Python Solutions/D20P2_LeetCode_1.py
+++ Python Solutions/D20P2_LeetCode_1.py
@@ -1,1 +1,8 @@
-# Python3 code coming soon
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_Index = {}
+        for idx, number in enumerate(nums):
+            if target - number in num_Index:
+                return num_Index[target - number], idx
+            num_Index[number] = idx
+
